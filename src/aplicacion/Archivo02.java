8a89ec98fa192fadc9b73cd8cc94db607bda4883
Eduardo-Rivas <edumeru46@gmail.com>
2020-04-09 20:23:10 -0300

    Tratamiento de Archivos 3

--- src/aplicacion/Archivo02.java
+++ src/aplicacion/Archivo02.java
@@ -11,22 +11,16 @@ public class Archivo02
  //--Metodo main--//
  public static void main(String[] args)
  {
-    //--Definimos objeto tipo FileReader inizaializado en null--//
-    FileReader fr = null;
-    BufferedReader br = null;
 
     //--Definimos una variable para la ruta del archivo--// 
     String ruta = "/home/eduardo/archivo.txt";
 
-    try
+    //--Instanciamos los objetos BufferReader y FileReader--//
+    //--Directamente en el bloque try, y no hace falta cerrarlos--//
+    try(BufferedReader br = new BufferedReader(new FileReader(ruta)))
     {
-       //--Instanciamos el objeto FileReader--//
-       fr = new FileReader(ruta);
 
-       //--Instanciamos el objeto BuffereReader apartir de fr--//
-       br = new BufferedReader(fr);
-
-       String linea = br.readLine();
+       String linea = br.readLine(); 
        while(linea != null)
        {
           System.out.println(linea);
@@ -36,28 +30,8 @@ public class Archivo02
     }
     catch(IOException e)
     {
-       System.out.println("Error..."+e.getMessage());
+       System.out.println("Error de Archivo..."+e.getMessage());
     }
-    finally
-    {
-       try
-       {
-          if(fr != null)
-          {
-             fr.close();
-          }
-          if(br != null)
-          {
-             br.close();
-          }
-       }
-       catch(IOException e)
-       {
-          e.printStackTrace();    
-       }
-
-    }
-   
 
  }
  
